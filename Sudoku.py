094b75aec2b4da3538ec4940b3792ff92382a125
blaze7frost <72850454+blaze7frost@users.noreply.github.com>
2021-01-03 14:27:09 +0530

    Update Sudoku.py

--- Sudoku.py
+++ Sudoku.py
@@ -1,15 +1,4 @@
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
-
+# solver.py
 
 def solve(bo):
     find = find_empty(bo)
@@ -75,9 +64,3 @@ def find_empty(bo):
                 return (i, j)  # row, col
 
     return None
-
-print_board(board)
-solve(board)
-print("@@@@@@@@@@@@@@@@@@@@@@@@")
-print("Solved Sudoku")
-print_board(board)
